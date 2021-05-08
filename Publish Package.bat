7a8443f6eb8e6c646ff4be9076c0806bde846656
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-09 01:32:23 +0800

    Backup main before priorityqueue-0.1.7

--- Publish Package.bat
+++ Publish Package.bat
@@ -1,16 +1,16 @@
 :: 设置项目名称
 SET ProjectName=Priority_Queue
-::设置模块名字
+:: 设置模块名字
 SET ToolName=upm-priorityqueue
-::设置模块版本
+:: 设置模块版本
 SET ToolVersion=priorityqueue-0.1.5
-::设置模块源路径（相对项目）
+:: 设置模块源路径（相对项目）
 SET ToolAssetPath=Assets/Package
 
 :: 进入项目文件夹
 cd %ProjectName%
 
-::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
+:: 此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
 git subtree split -P %ToolAssetPath% --branch %ToolName%
 
 :: 推送到远端
@@ -35,7 +35,7 @@ git checkout %ToolName%
 for /F %%i in ('git rev-parse --short HEAD') do ( set commitid=%%i)
 git tag -a %ToolVersion% %commitid% -m "%ToolVersion%"
 
-::提交
+:: 提交
 git push --set-upstream origin upm-priorityqueue
 git push --tags
 
