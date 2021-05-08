cfb44c20871845fb7df38bdc015447a81c305f16
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-08 16:10:24 +0800

    Move the All the files in the package to the Package folder
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Publish Package.bat
+++ Publish Package.bat
@@ -3,7 +3,7 @@ SET ToolName=upm-priorityqueue
 ::设置模块版本
 SET ToolVersion=priorityqueue-0.1.3
 ::设置模块源路径
-SET ToolAssetPath=Assets/
+SET ToolAssetPath=Assets/Package
 
 ::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
 git subtree split -P %ToolAssetPath% --branch %ToolName%
@@ -13,5 +13,6 @@ git tag %ToolVersion% %ToolName%
 :: 推送到远端
 git push origin %ToolName% %ToolVersion%
 git push origin %ToolName%
-git subtree push --prefix=%ToolAssetPath% origin %ToolName%
+
+git subtree push --tags --prefix=%ToolAssetPath% origin %ToolName% %ToolVersion%
 pause
