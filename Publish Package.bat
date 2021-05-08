f14809c25c70be4e104d9ab40419b40714ee1fdc
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-08 17:26:04 +0800

    priorityqueue-0.1.3

--- Publish Package.bat
+++ Publish Package.bat
@@ -7,22 +7,29 @@ SET ToolVersion=priorityqueue-0.1.3
 ::设置模块源路径
 SET ToolAssetPath=Assets/Package
 
+:: 进入项目文件夹
+cd %ProjectName%
+
 ::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
 git subtree split -P %ToolAssetPath% --branch %ToolName%
 
 :: 推送到远端
-git push origin %ToolName% %ToolVersion%
 git push origin %ToolName%
 
-git checkout main
+:: 进入package分支并进行更新
+git checkout -b package
+git checkout package
 git add -A
 git commit -m "%ToolVersion%"
 
+:: 将所有更新提交到ToolName分支
 git subtree push --prefix=%ToolAssetPath% origin %ToolName%
 
+:: 进入ToolName并提交tag
 git checkout %ToolName%
 git tag -a %ToolVersion% -m "%ToolVersion%"
 
+:: 回到main分支并进行最后的提交
 git checkout main
 
 git push origin %ToolName% %ToolVersion%
