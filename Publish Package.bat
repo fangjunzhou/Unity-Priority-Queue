45360a00d3d0c7d00d310c0d6d0c817bda061950
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-05-08 15:28:18 +0800

    Remove the TextMeshPro
    Signed-off-by: Fangjun Zhou <fangjun.zhou02@gmail.com>

--- Publish Package.bat
+++ Publish Package.bat
@@ -1,14 +1,21 @@
 ::设置模块名字
 SET ToolName=upm-priorityqueue
 ::设置模块版本
-SET ToolVersion=priorityqueue-0.1.1
+SET ToolVersion=priorityqueue-0.1.3
 ::设置模块源路径
 SET ToolAssetPath=Assets/
 
-::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
-git subtree split -P %ToolAssetPath% --branch %ToolName%
-:: 在ToolName分支设置标签ToolVersion节点
-git tag %ToolVersion% %ToolName%
+::此命令会创建一个ToolVersion的分支，并同步ToolAssetPath下的内容
+git subtree split -P %ToolAssetPath% --branch tmp
+:: 在ToolVersion分支设置标签ToolVersion节点
+git tag %ToolVersion% tmp
+
+:: Checkout到ToolName，把更新的内容merge进来
+git branch %ToolName%
+git checkout %ToolName%
+git merge tmp
+
+git branch -D tmp
 
 :: 推送到远端
 git push origin %ToolName% %ToolVersion%
